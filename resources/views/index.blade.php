8db3908fef6a56ffdd14f2d7d535f0a6ad761541
yuminyu <pamama.actor@gmail.com>
2022-08-15 10:19:12 +0900

    aaa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+@if(Auth::check())
+<div class="card">
+    <div class="card-header">{{ __('あなたはログイン状態') }}</div>
+
+    <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        {{ __('ここはログイン後のTOP画面です') }}
+    </div>
+</div>
+@else
+<div class="card">
+    <div class="card-header">{{ __('あなたはログインしていない状態') }}</div>
+
+    <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        {{ __('ログインしましょう') }}
+    </div>
+</div>
+@endif
+
 <div class="container">
     <div class="row">
         <div class="col">
@@ -11,6 +39,7 @@
         </div>
     </div>
 </div>
+
 @if(Auth::check())
     <div>
         <p><a href="/majan/top">ログイン後のTop画面にいく</a></p>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-
+    
     <div class="card">
         <div class="card-header">{{ __('あなたはログイン状態') }}</div>
 
