0cc39bd4a05b636b81d1b6dead08f51429b48e4d
yuminyu <pamama.actor@gmail.com>
2022-08-15 10:13:24 +0900

    aaa

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,32 +2,59 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="card">
+        <div class="card-header">{{ __('あなたはログイン状態') }}</div>
 
-                    {{ __('You are logged in!') }}
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
-            <div class="card">
-                <p><a href="/majan/listattendant">アテンダント一覧画面に</a></p>
-                <p><a href="/majan/recommend">オススメ雀荘</a></p>
-                <p><a href="/majan/attendantregister">アテンダントとして登録する</a></p>
-            <div>
-            @if($user_kengen === 5)
-            <div class="card">
-                <p><a href="/majan/yameru">アテンダントを辞める</a></p>
-            <div>
             @endif
+
+            {{ __('ここはログイン後のTOP画面です') }}
+            <br>{{ __('ここはログイン後のTOP画面です') }}
         </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="row">
+            <div class="col">
+                <p><img src="{{ asset('img/welcom.jpg') }}" alt="" class="my-5 w-100"></p>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <p><a href="/majan/listattendant">アテンダント一覧画面に</a></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p><img src="{{ asset('img/welcom.jpg') }}" alt="" class="my-5 w-100"></p>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <p><a href="/majan/recommend">オススメ雀荘</a></p>
+            </div>
+        </div>
+        @if($user_kengen === 1)
+        <div class="row">
+            <div class="col">
+                <p><img src="{{ asset('img/welcom.jpg') }}" alt="" class="my-5 w-100"></p>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <p><a href="/majan/attendantregister">アテンダントとして登録する</a></p>
+            </div>
+        </div>
+        @endif
+        @if($user_kengen === 5)
+        <div class="row">
+            <div class="col">
+                <p><img src="{{ asset('img/welcom.jpg') }}" alt="" class="my-5 w-100"></p>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center">
+                <p><a href="/majan/yameru">アテンダントを辞める</a></p>
+            </div>
+        </div>
+        @endif
     </div>
 </div>
 @endsection
